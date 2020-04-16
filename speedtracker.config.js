7190fd7642c5ee0d3597e5e907772cf38d343eaa
Mathias Maier <hello@mathiasmaier.de>
2020-04-16 14:38:47 +0200

    Update speedtracker.config.js

--- speedtracker.config.js
+++ speedtracker.config.js
@@ -1,18 +1,53 @@
 const config = {
   profiles: {
     // Example profile
-    test: {
-      name: 'Tidal test',
+    webapp: {
+      name: 'TIDAL Web App',
       parameters: {
         connectivity: 'Cable',
-        location: 'London_EC2',
-        runs: 5,
+        location: 'Dulles:Chrome',
+        runs: 1,
         url: 'https://listen.tidal.com'
       },
       budgets: [
         {
-          metric: 'TTFB',
-          max: 1000
+          metric: 'SpeedIndex',
+          max: 3000
+        },
+        {
+          metric: 'loadTime',
+          max: 5000
+        },
+        {
+          metric: 'fullyLoaded',
+          max: 10000
+        }
+      ]
+    },
+    website: {
+      name: 'TIDAL.com',
+      parameters: {
+        connectivity: 'Cable',
+        location: 'Dulles:Chrome',
+        runs: 1,
+        url: 'https://tidal.com'
+      },
+      budgets: [
+        {
+          metric: 'SpeedIndex',
+          max: 3000
+        },
+        {
+          metric: 'domInteractive',
+          max: 3000
+        },
+        {
+          metric: 'loadTime',
+          max: 5000
+        },
+        {
+          metric: 'fullyLoaded',
+          max: 10000
         }
       ]
     }
